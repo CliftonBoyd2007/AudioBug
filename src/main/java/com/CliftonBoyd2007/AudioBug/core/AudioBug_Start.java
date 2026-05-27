72d179b67fdb7390719f46c1d350db5bdc80b360
CliftonBoyd2007 <cboyd32007@gmail.com>
2026-05-27 14:27:15 -0400

    Remove shell for screen reader announcement when files fail to load on startup.

--- src/main/java/com/CliftonBoyd2007/AudioBug/core/AudioBug_Start.java
+++ src/main/java/com/CliftonBoyd2007/AudioBug/core/AudioBug_Start.java
@@ -1,14 +1,31 @@
 package com.CliftonBoyd2007.AudioBug.core;
 
-import com.CliftonBoyd2007.AudioBug.audioutils.CuePlayer;
+
+
+
 import com.intellij.openapi.startup.StartupActivity;
+import com.intellij.openapi.project.Project;
 
+import java.io.File;
 
-public class AudioBugStart implements StartupActivity {
+import com.CliftonBoyd2007.AudioBug.audioutils.CuePlayer;
+import org.jetbrains.annotations.NotNull;
+
+public class AudioBug_Start implements StartupActivity {
+
+    /**
+     * Initializes file resources and instantiates {@link CuePlayer} object for cue playback.
+     *
+     * @param project The currently open project.
+     */
     @Override
-    public void runActivity(Project project) {
-        
-    }
+    public void runActivity(@NotNull Project project) {
+        ClassLoader cl = this.getClass().getClassLoader();
+        File cue_Breakpoint = new File(String.valueOf(cl.getResource("Sounds/Breakpoint.wav")));
+        File cue_Error = new File(String.valueOf(cl.getResource("Sounds/Error.wav")));
+        File cue_Warning = new File(String.valueOf(cl.getResource("Sounds/Warning.wav")));
+        CuePlayer cuePlayer = new CuePlayer(cue_Breakpoint, cue_Error, cue_Warning);
 
+    }
 
 }
