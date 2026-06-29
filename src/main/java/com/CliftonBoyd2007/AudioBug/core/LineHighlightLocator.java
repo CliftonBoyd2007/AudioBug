668348b822ed904bd5e4f9b76e9f377e73347e6e
CliftonBoyd2007 <cboyd32007@gmail.com>
2026-06-29 11:09:22 -0400

    Add primitive retrieval of HighlightInfos for the current line of the caret.

--- src/main/java/com/CliftonBoyd2007/AudioBug/core/LineHighlightLocator.java
+++ src/main/java/com/CliftonBoyd2007/AudioBug/core/LineHighlightLocator.java
@@ -1,11 +1,19 @@
 package com.CliftonBoyd2007.AudioBug.core;
 
+import java.util.ArrayList;
+
+import com.intellij.lang.annotation.HighlightSeverity;
+import com.intellij.util.Processor;
+import com.intellij.codeInsight.daemon.impl.DaemonCodeAnalyzerEx;
+import com.intellij.codeInsight.daemon.impl.HighlightInfo;
+
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.event.CaretEvent;
+import com.intellij.util.ui.accessibility.AccessibleAnnouncerUtil;
 
-import com.intellij.openapi.editor.markup.MarkupModel;
-import com.intellij.openapi.editor.markup.RangeHighlighter;
+import com.intellij.openapi.project.Project;
+
 import org.jetbrains.annotations.NotNull;
 
 public class LineAnnotationLocator {
@@ -16,9 +24,10 @@ public class LineAnnotationLocator {
      * Record containing the start and end offsets for the current line of the caret.
      */
     private LineOffsets lineOffsets;
+    private ArrayList<HighlightInfo> highlights;
 
     public LineAnnotationLocator(@NotNull CaretEvent event) {
-
+        highlights = new ArrayList<>();
         update(event);
 
 
@@ -39,7 +48,7 @@ public class LineAnnotationLocator {
     }
 
     /**
-     * Updates the line offsets field when the caret is moved.
+     * Updates the line offsets field when the caret is moved to a new line.
      *
      * @param event The event containing information about the caret.
      */
@@ -47,13 +56,19 @@ public class LineAnnotationLocator {
         this.lineOffsets = getLineOffsets(event);
     }
 
-
     /**
-     * This is simply a playground. Remove this in public code.
+     * Adds highlights to a backing store for on-demand retrieval by the user.
+     *
+     * @param document The document from which highlights are requested.
+     * @param project  The project from which the document originates.
      */
+    private void getHighlightHelper(Document document, Project project) {
 
-    private void playground(@NotNull CaretEvent event) {
 
+        DaemonCodeAnalyzerEx.processHighlights(document, project, HighlightSeverity.WARNING, lineOffsets.startOffset, lineOffsets.endOffset, Processor < HighlightInfo > processor, info -> {
+            this.highlights.add(info);
+            return true;
+        });
     }
 
 
