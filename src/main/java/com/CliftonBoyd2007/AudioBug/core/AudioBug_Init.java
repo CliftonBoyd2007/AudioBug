5b967ff36517cdd2620241a2abf594e61fd824ea
CliftonBoyd2007 <cboyd32007@gmail.com>
2026-07-11 14:43:54 -0400

    Replace AudioBug_Start with AudioBug_init and update plugin.xml to register AudioBug_init to editorFactoryListener extension point.

---
+++ src/main/java/com/CliftonBoyd2007/AudioBug/core/AudioBug_Init.java
@@ -0,0 +1,15 @@
+package com.CliftonBoyd2007.AudioBug.core;
+
+import com.intellij.openapi.editor.CaretModel;
+import com.intellij.openapi.editor.event.EditorFactoryEvent;
+import com.intellij.openapi.editor.event.EditorFactoryListener;
+import org.jetbrains.annotations.NotNull;
+
+public class AudioBug_Init implements EditorFactoryListener {
+    @Override
+    public void editorCreated(@NotNull EditorFactoryEvent event) {
+        CaretModel caretModel = event.getEditor().getCaretModel();
+        caretModel.addCaretListener(new CaretWatcher());
+
+    }
+}
