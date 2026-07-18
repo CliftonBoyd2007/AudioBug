c215785bcedd2c5728a41a8d1b14f5d1bb57c5bf
CliftonBoyd2007 <cboyd32007@gmail.com>
2026-07-18 16:09:53 -0400

    Add Javadoc comments to the entire of WatchCaret and its methods.

--- src/main/java/com/CliftonBoyd2007/AudioBug/core/WatchCaret.java
+++ src/main/java/com/CliftonBoyd2007/AudioBug/core/WatchCaret.java
@@ -6,35 +6,55 @@ import com.intellij.openapi.editor.event.CaretListener;
 
 
 import org.jetbrains.annotations.NotNull;
-import com.CliftonBoyd2007.AudioBug.audioutils.CuePlayer;
+
 
 import java.io.File;
 
-
+/**
+ * Listens to caret movement and determines whether AudioBug should respond when the caret changes position.
+ *
+ * <p>
+ * It distinguishes between vertical and lateral movement of the caret to avoid overwhelming the user with feedback.
+ *
+ * @author Clifton Boyd
+ */
 public class WatchCaret implements CaretListener {
-    private CuePlayer player;
 
-
+    /**
+     * Constructs an instance of the WatchCaret class.
+     * <p>
+     * Please do not call this yourself. This is only here for {@link AudioBug_Init}.
+     */
     WatchCaret() {
         // Unless you are testing something, leave this method alone.
         // This only exists to make sure that this object is correctly constructed.
-        this.player = new CuePlayer();
+
     }
 
+    /**
+     * Responds to caret events within the editor.
+     * When the caret moves, it determines if the caret has moved to a new line.
+     * <p>
+     * With that information, it responds accordingly.
+     *
+     * @param event the event containing information about the caret.
+     */
     @Override
     public void caretPositionChanged(@NotNull CaretEvent event) {
         LogicalPosition oldPosition = event.getOldPosition();
         LogicalPosition newPosition = event.getNewPosition();
         boolean caretHasMovedToNewLine = hasCaretMovedToNewLine(oldPosition, newPosition);
         if (caretHasMovedToNewLine) {
-            this.player.playCue_Breakpoint();
+
         } else {
-            this.player.playCue_Warning();
+            return;
+
         }
     }
 
     /**
      * Indicates whether the caret has moved to a new line.
+     *
      * @param oldPosition The previous position of the caret.
      * @param newPosition The current position of the caret.
      * @return true if the line of the caret has changed.
