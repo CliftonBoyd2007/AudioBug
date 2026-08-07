371f70e6080642d1a1bc9347091cdec2a4f1bcb2
CliftonBoyd2007 <cboyd32007@gmail.com>
2026-08-07 16:43:51 -0400

    Add screen reader announcement for AudioBug startup.

--- src/main/java/com/CliftonBoyd2007/AudioBug/core/AudioBug_Init.java
+++ src/main/java/com/CliftonBoyd2007/AudioBug/core/AudioBug_Init.java
@@ -44,6 +44,7 @@ public final class AudioBug_Init implements EditorFactoryListener {
     private void audioBugStartupNotifier(FeedbackService feedbackService) {
         if (!this.notifiedUserOfStartup) {
             feedbackService.player.playCue_Start();
+            feedbackService.announce("AudioBug is ready.", true);
             this.notifiedUserOfStartup = true;
         } else {
             return;
